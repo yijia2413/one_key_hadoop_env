2483e7dd742ce1e8f20f44b6ad67232121697709
yijia <yijia2413@gmail.com>
2015-05-31 11:14:17 +0800

    set path and classpath

---
+++ dev/run_on_command_line.sh
@@ -0,0 +1,50 @@
+#########################################################################
+# File Name: run_on_command_line.sh
+# Author: yijia
+# mail: yijia2413@gmail.com
+# Created Time: Sun May 31 11:11:58 2015
+#########################################################################
+
+#thanks for zwlin
+
+if [ $# -lt 1 ] ; then
+   echo "Usage: $0 <java file name>";
+   exit 
+fi
+echo $#
+
+#check hadoop_home
+if [ -d "$HADOOP_HOME" ] ; then
+	echo "HADOOP_HOME : $HADOOP_HOME"
+else 
+	echo "you should set HADOOP_HOME first !"
+	exit
+fi
+
+#check hbase_home
+if [ -d "$HBASE_HOME" ] ; then
+	echo "HBASE_HOME : $HBASE_HOME"
+else
+	echo "you should set HBASE_HOME first !"
+	exit
+fi
+
+#add hadoop libs
+for i in $HADOOP_HOME/share/hadoop/* ; do
+	for j in $i/*.jar ; do
+		export CLASSPATH=$CLASSPATH:$j;
+	done;
+	for j in $i/lib/*.jar ; do 
+		export CLASSPATH=$CLASSPATH:$j;
+	done;
+done;
+
+#add hbase libs
+for i in $HBASE_HOME/lib/*.jar ; do 
+	export CLASSPATH=$CLASSPATH:$i;
+done;
+
+#echo $CLASSPATH
+
+#set your command below...
+
